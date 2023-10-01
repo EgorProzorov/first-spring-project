5b2bba384543231f19deb3bdf82f6fc119f66a4c
EgorProzorov <9852602994@mail.ru>
2023-10-01 17:22:05 +0300

    first commit

---
+++ src/test/java/com/tours/travel/agency/TravelAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tours.travel.agency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAgencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
