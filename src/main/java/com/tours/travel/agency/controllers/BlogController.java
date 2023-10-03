d3869df8e3caa368dc423d200a424291585b720b
EgorProzorov <9852602994@mail.ru>
2023-10-03 19:21:13 +0300

    errors with connecting mysql

--- src/main/java/com/tours/travel/agency/controllers/BlogController.java
+++ src/main/java/com/tours/travel/agency/controllers/BlogController.java
@@ -1,2 +1,22 @@
-package com.tours.travel.agency.controllers;public class BlogController {
+package com.tours.travel.agency.controllers;
+
+import com.tours.travel.agency.models.Post;
+import com.tours.travel.agency.repo.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BlogController {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @GetMapping("/blog")
+    public String blogMain(Model model){
+        Iterable<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
+        return "blog-main";
+    }
 }
